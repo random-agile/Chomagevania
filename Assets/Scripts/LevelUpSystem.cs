f30515c41e27e43d7176636828f33a87bd010251
random-agile <o.miraciski@gmail.com>
2022-12-23 12:37:33 +0100

    LevelUpSystem Part 2

--- Assets/Scripts/LevelUpSystem.cs
+++ Assets/Scripts/LevelUpSystem.cs
@@ -6,20 +6,43 @@ public class LevelUpSystem : MonoBehaviour
 {
 	List<string> weapons;
 	List<int> weaponsLvl;
-	List<bool> slotTakenW;
+	public List<bool> slotTakenW;
 	List<string> passives;
 	List<int> passivesLvl;
-	List<bool> slotTakenP;
+	public List<bool> slotTakenP;
+	int chosenSlot;
+	int index;
+	int actualLevel;
 	
 	public List<GameObject> weaponPref;
 	public List<GameObject> weaponIcon;
-	public List<GameObject> passivesPref;
+	public List<GameObject> passivesIcon;
 	
+	public List<Transform> canvasStockerW;
+	public List<Transform> canvasStockerP;
+	
+	public Transform playerPos;
+	
+	PlayerStats PS;
+	
+	
+	void Awake()
+	{		
+		weapons = new List<string>();
+		weaponsLvl = new List<int>();
+		passives = new List<string>();
+		passivesLvl = new List<int>();
+		PS = GameObject.FindWithTag("Player").GetComponent<PlayerStats>();
+	}
 	
 	void Start()
 	{
-		weapons.Add("MagicBalls");
-		weaponsLvl.Add(0);
+		AddNewWeapon("MagicBalls");
+		CheckLevelWeapon("MagicBalls");
+		LevelUpWeapon("MagicBalls", actualLevel);
+		AddNewPassive("HPMax");
+		CheckLevelPassive("HPMax");
+		LevelUpPassives("HPMax", actualLevel);
 	}
 	
 	void AddNewWeapon(string weaponName)
@@ -27,11 +50,88 @@ public class LevelUpSystem : MonoBehaviour
 		switch(weaponName)
 		{
 		case "MagicBalls":
-			Instantiate(weaponPref[0]);
+			Instantiate(weaponPref[0], playerPos);
 			weapons.Add("MagicBalls");
 			weaponsLvl.Add(0);
+			CheckSlotWeapon();
+			Instantiate(weaponIcon[0], canvasStockerW[chosenSlot]);
 			break;
 		}
+	}
+	
+	void AddNewPassive(string passiveName)
+	{
+		switch(passiveName)
+		{
+		case "HPMax":
+			PS.hpMax+=20;
+			PS.hpSlider.maxValue = PS.hpMax;
+			passives.Add("HPMax");
+			passivesLvl.Add(0);
+			CheckSlotPassive();
+			Instantiate(passivesIcon[0], canvasStockerP[chosenSlot]);
+			break;
+		}
+	}
+	
+	
+	
+	void CheckSlotWeapon()
+	{
+		for(int i = 0; i < 5; i++)
+		{
+			if(slotTakenW[i])
+			{
+				Debug.Log(slotTakenW[i]);
+			}
+			
+			else if(!slotTakenW[i])
+			{
+				slotTakenW[i] = true;
+				chosenSlot = i;
+				return;
+			}
+			
+			if(slotTakenW.TrueForAll(x => x))
+			{
+				Debug.Log("full");
+			}			
+		}
+	}
+	
+	void CheckSlotPassive()
+	{
+		for(int i = 0; i < 5; i++)
+		{
+			if(slotTakenP[i])
+			{
+				Debug.Log(slotTakenP[i]);
+			}
+			
+			else if(!slotTakenP[i])
+			{
+				slotTakenP[i] = true;
+				chosenSlot = i;
+				return;
+			}
+			
+			if(slotTakenP.TrueForAll(x => x))
+			{
+				Debug.Log("full");
+			}			
+		}
+	}
+	
+	void CheckLevelWeapon(string weaponName)
+	{
+		index = weapons.IndexOf(weaponName);
+		actualLevel = weaponsLvl[index];
+	}
+	
+	void CheckLevelPassive(string passiveName)
+	{
+		index = passives.IndexOf(passiveName);
+		actualLevel = passivesLvl[index];
 	}
 	
 	void LevelUpWeapon(string weaponName, int weaponLevel)
@@ -43,19 +143,24 @@ public class LevelUpSystem : MonoBehaviour
 			{
 			case "MagicBalls":
 				RotateBall RB = GameObject.Find("MagicBalls(Clone)").GetComponent<RotateBall>();
-				RB.wSpeed += 50f;
+				RB.wSpeed += 1f;
+				index = weapons.IndexOf("MagicBalls");
+				weaponsLvl[index]+=1;
 				break;
 			case "Tornado":
 				GameObject tornado = GameObject.Find("Tornado(Clone)");
 				tornado.transform.localScale += new Vector3(0.1f,0.1f,0.1f);
+				weaponsLvl[index]+=1;
 				break;
 			case "Katana":
 				Katana K = GameObject.Find("Katana(Clone)").GetComponent<Katana>();
 				K.katanaCooldown += 5;
+				weaponsLvl[index]+=1;
 				break;
 			case "FlameThrow":
 				FlameThrow FT = GameObject.Find("FlameThrow(Clone)").GetComponent<FlameThrow>();
 				FT.weaponDuration += 20;
+				weaponsLvl[index]+=1;
 				break;
 			case "Laser":
 				break;
@@ -132,7 +237,11 @@ public class LevelUpSystem : MonoBehaviour
 		case 0:
 			switch(passiveName)
 			{
-			case "a":
+			case "HPMax":
+				PS.hpMax+=20;
+				PS.hpSlider.maxValue = PS.hpMax;
+				index = passives.IndexOf("HPMax");
+				passivesLvl[index]+=1;
 				break;
 			case "b":
 				break;
